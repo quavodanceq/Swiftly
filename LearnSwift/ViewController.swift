869607230c1d8327beb20c9f3ece974dd8f235aa
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-09-25 20:53:58 +0600

    Initial Commit

---
+++ LearnSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by Куат Оралбеков on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
