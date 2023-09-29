87fba40b4bbf17db8502d3fb5fb2d391d86b229f
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-09-29 22:02:39 +0600

    return to main vc after practice solving added + animated

--- LearnSwift/ViewControllers/CodeViewController.swift
+++ LearnSwift/ViewControllers/CodeViewController.swift
@@ -45,8 +45,22 @@ class CodeViewController: UIViewController {
     @objc private func finishButtonTapped() {
         
         UDManager.shared.passed(topicNumber: topic!.number)
-        self.navigationController?.popViewController(animated: true)
-        self.navigationController?.parent?.viewDidLoad()
+        let snapShot = UIView()
+        if let scene = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
+            let mainVC = UINavigationController(rootViewController: ViewController())
+            scene.window?.rootViewController = mainVC
+            UIView.transition(
+                with: scene.window!,
+                    duration: 0.5,
+                    options: .transitionCrossDissolve,
+                    animations: {
+                        snapShot.transform = CGAffineTransform(translationX: 0, y: snapShot.frame.height)
+                    },
+                    completion: { status in
+                        snapShot.removeFromSuperview()
+                    }
+                )
+        }
     }
     
     private func setupTaskLabel() {
